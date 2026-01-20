dcd62ba562809bc35b6ca5e482fd44dbda23cfaf
darbyhtml <darbyforbest@gmail.com>
2026-01-21 00:03:59 +0300

    Исправлен импорт timezone в views.py

--- backend/mailer/views.py
+++ backend/mailer/views.py
@@ -10,6 +10,7 @@ from django.http import HttpRequest, HttpResponse
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from django.urls import reverse
+from django.utils import timezone
 
 from accounts.models import User
 from audit.models import ActivityEvent
